fb623edaf32da2bae912abe7fadab680fc25f21f
Nayan1911 <sharmanayan39@gmail.com>
2023-11-01 18:45:58 +0530

    test

--- src/main/java/com/nayan/cicd/TestApplication.java
+++ src/main/java/com/nayan/cicd/TestApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class TestApplication {
-    @GetMapping("/welcome"")
+    @GetMapping("/welcome")
     public String welcome(){
         return "welcome";
     }
